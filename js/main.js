c7a0381ae7c5de820303cfa1f60012368ddcdeb7
Trần Đăng Tùng <trandtung1209@gmail.com>
2022-03-12 16:05:49 +0700

    init cell el list

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+import { TURN } from "./constants.js";
 import {
   getCellElementList,
   getGameStatusElement,
@@ -8,7 +9,7 @@ import {
 /**
  * Global variables
  */
-let currentTurn = "cross";
+let currentTurn = TURN.CROSS;
 let isGameEnded = false;
 let cellValues = new Array(9).fill("");
 
@@ -27,3 +28,37 @@ let cellValues = new Array(9).fill("");
  * 4. On replay button click --> reset game to play again.
  *
  */
+function toggleTurn() {
+  currentTurn = currentTurn === TURN.CIRCLE ? TURN.CROSS : TURN.CIRCLE;
+
+  //update turn DOM
+  const currentTurnElement = getCurrentTurnElement();
+  if (currentTurnElement) {
+    currentTurnElement.classList.remove(TURN.CIRCLE, TURN.CROSS);
+    currentTurnElement.classList.add(currentTurn);
+  }
+}
+
+function handleCellClick(cell, index) {
+  const isClick =
+    cell.classList.contains(TURN.CIRCLE) || cell.classList.contains(TURN.CROSS);
+  if (isClick) return;
+  //set selected cell
+  cell.classList.add(currentTurn);
+  //toggle X O
+  toggleTurn();
+  console.log("click", cell, index);
+}
+
+function initCellElementList() {
+  const cellElementList = getCellElementList();
+  cellElementList.forEach((cell, index) => {
+    cell.addEventListener("click", () => handleCellClick(cell, index));
+  });
+}
+
+(() => {
+  // bind click event for li element
+  initCellElementList();
+  // bind click event Reply button
+})();
